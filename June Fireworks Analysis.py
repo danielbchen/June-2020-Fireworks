7f915c23c3d24715d2e7e4ce8534227c45dff691
danielbchen <danielbchen@users.noreply.github.com>
2020-06-24 10:38:27 -0500

    Initial analysis of June 2020

---
+++ June Fireworks Analysis.py
@@ -0,0 +1,59 @@
+# Import packages for data analysis and visualization
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sodapy import Socrata
+
+# Load 311 NYC fireworks complaing data. Publicly available at:
+# https://data.cityofnewyork.us/Social-Services/311-Fireworks-Complaints/g4u2-tvag
+
+# Use unathenticated client with publicly avaibale data set
+# Return entire data set
+client = Socrata('data.cityofnewyork.us', None)
+results = client.get_all('g4u2-tvag')
+
+# Convert data to pandas DataFrame
+fireworks_df = pd.DataFrame.from_records(results)
+
+# Convert 'created_date' column to datetime object to plot
+fireworks_df['created_date'] = pd.to_datetime(fireworks_df['created_date'], errors='coerce')
+
+# Create a new dummy column. If 'complaint_type' equals illegal fireworks
+# then the column will contain a 1. Otherwise, it will be zero.
+fireworks_df['fireworks'] = [1 if complaint == 'Illegal Fireworks' else 0 for
+complaint in fireworks_df['complaint_type']]
+
+# Group incidents by date of occurance.
+fireworks_grouped = fireworks_df.groupby(pd.Grouper(key='created_date', freq='D')).sum().reset_index()
+
+# Renme columns for clarity
+fireworks_grouped.columns = ['Incident Date', 'Number of Reports']
+
+# Select the incidents from the beginning of June to the most recent data
+fireworks = fireworks_grouped[(fireworks_grouped['Incident Date'] >= '2020-06-01') & (fireworks_grouped['Incident Date'] < '2020-06-23')]
+
+# Drop the timestape from the 'Incident Date' column
+pd.options.mode.chained_assignment = None
+fireworks['Incident Date'] = pd.to_datetime(fireworks['Incident Date']).dt.date
+
+# Convert the columns to a list object in order to graph
+Date = list(fireworks['Incident Date'])
+Count = list(fireworks['Number of Reports'])
+
+# Set figure dimensions and create graph
+plt.figure(figsize=(10,7))
+plt.plot(Date, Count, color='red', alpha=0.6, linewidth=2)
+
+# Add title and axis labels
+plt.title('Illegal Fireworks Reported to 311 in June 2020', fontsize=16)
+plt.xlabel('Date', fontsize=12)
+plt.ylabel('Number of Reports', fontsize=12)
+
+# Formatting changes below
+plt.margins(x=0, y=0) # Removes extra white space between spines and graph
+sns.despine() # Removes the top and right spine
+
+# Show and save plot
+plt.savefig('Fireworks Graph.png')
+plt.show()
